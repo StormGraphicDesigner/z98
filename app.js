4f98b83cd601ee91a9e4764341bcdef27a45e72a
Munkhbayr <munkhbayrduuren@gmail.com>
2021-12-21 23:12:21 +0800

    alert

--- app.js
+++ app.js
@@ -7,3 +7,8 @@
         hamburger.classList.toggle("active-toggle")
 
     })
+    // function mendchileh(){
+    //     alert("Сайн уу.\n\nМиний веб сайтад\n\nТавтай морилно уу")
+    // }
+
+    setTimeout(alert("Тавтай морилно уу"), 5000);
